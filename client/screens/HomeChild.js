dafe998eba0c15e56d517aba9d99d5c33c0a485d
Ahlem5br <benromdhaneahlem84@gmail.com>
2023-01-19 03:09:08 +0100

    rewards done with animated congratulation

--- client/screens/HomeChild.js
+++ client/screens/HomeChild.js
@@ -1,124 +1,87 @@
 
-// ////5idmet ahlem tasks child illi imiss no9tlouuuuuuuuuuu//////////////
+
+
+//ahlem
 
 import * as React from "react";
-import {  Component, createRef ,useState,useEffect } from "react";
-import { View, Text, StatusBar, style, Pressable,Platform, StyleSheet, TouchableOpacity,Button } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, StatusBar,Keyboard } from "react-native";
 import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
 import { firebase } from "../config/firebase.js";
-import BouncyCheckbox from "react-native-bouncy-checkbox";
-import RewardsComponent from 'react-native-rewards';
-
-
-const CornflowerBlue = '#6495ED';
 
 const colors = {
-  themeColor: "#00BFA6",
-  white: "#fff",
-  background: "#f4f6fc",
-  greyish: "#a4a4a4",
-  tint: "#2b49c3",
-};
-// dummy data
-const Lists = [
-  {
-    task: "Do your homework page 90",
-    icon: "hiking",
-    theme: "#37003c",
-    stamp: "Today . 8pm",
-  },
-  {
-    task: "Take care of your sister",
-    icon: "account-tie",
-    theme: "#008b8b",
-    stamp: "Today . 8pm",
-  },
-  {
-    task: "Go to school at 10am",
-    icon: "cart",
-    theme: "#fed132",
-    stamp: "Today . 8pm",
-  },
-  {
-    task: "It's lunch time",
-    icon: "weight",
-    theme: "#fed132",
-    stamp: "Today . 12am",
-  },
-];
+    themeColor: "#00BFA6",
+    white: "#fff",
+    background: "#f4f6fc",
+    greyish: "#a4a4a4",
+    tint: "#2b49c3",
+  };
 
+// fel props must add id parent: props.idP
 export default function List(props) {
-  // Declaring const where to save data
-  //Delete from firestore
-  //fetch data
-  const [todos, setTodos] = useState([]);
-
-  // collect collection tasks from firebase
-  const todoRef = firebase.firestore().collection("tasks");
-
+ 
+  const [rwd, setRwd] = useState([]);
+  const todoRef = firebase.firestore().collection("rewards");
+ 
   const [addData, setAddData] = useState("");
-  const [animationState,setAnimationState] =useState('rest')
 
   //Get data from firestore
   useEffect(() => {
-      setAnimationState("reward")
-
-    todoRef.orderBy("stamp", "desc").onSnapshot((QuerySnapshot) => {
-      const todos = [];
+    
+    todoRef.onSnapshot((QuerySnapshot) => {
+      const rwd = [];
       QuerySnapshot.forEach((doc) => {
         //  console.log(doc)
-        const { task, icon, theme, stamp,check,idChild,idParent} = doc.data();
+        const { description, idChild } = doc.data();
         // console.log(doc.data())
+        // condition to verify the child id
         // if (idChild==="222"){
-        todos.push({
+        rwd.push({
           id: doc.id,
-          task,
-          icon,
-          theme,
-          stamp,
-          check,
-          idChild,
-          idParent,
+          description,
+          
         });
-    // }
+      // }
       });
-      setTodos(todos);
+      setRwd(rwd);
     });
-   
   }, []);
-  
-  // function to check true or false
-  const updateCheck = (task) => {
-    todoRef.doc(task.id).update({
-      check: !task.check,
-    }).then(()=>{
-    console.log(task)
-   })
-  
-    .catch(error=>alert('ee',error))
-  };
+ 
 
-  // function linefalse
-  const linefalse=(task)=>{
-    if (task.check===true){
-        return "line-through"
-    }
-    return "none"
-  }
-  
+  // function add a tod
+  // hou
+//   const addTodo = () => {
+//     // check if we have a todo
+//     if (addData && addData.length > 0) {
+//       // get the timestamp
+//       const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+//       const data = {
+//         task: addData,
+//         icon: "child",
+//         theme: "#00BFA6",
 
 
-//function rewards
-const rewards = ()=> {
-    var res=true
-    for (var i =0 ;i<todos.length;i++){
-         if (todos[i].check===false){
-       res= false
-       }
-     }
-   console.log('all todos are done')
-     return  res   }
+//         stamp: timestamp,
+//         check:false,
+//         // add id parent child
+//         idParent:'111',
+//         idChild:'222',
+//       };
+//       todoRef
+//         .add(data)
+//         .then(() => {
+//           setAddData("");
+//           //  release keyboard
+//            Keyboard.dismiss();
+//         })
+//         .catch((error) => {
+//           alert(error);
+//         });
+//     }
+//   };
+ 
+
   return (
     <View
       style={{
@@ -128,9 +91,7 @@ const rewards = ()=> {
     >
       {/* display data from firebase */}
       <StatusBar barStyle="light-content" backgroundColor={colors.themeColor} />
-     {rewards() ===false ?(
-     <View>
-     <View style={{ backgroundColor: colors.themeColor }}>
+      <View style={{ backgroundColor: colors.themeColor }}>
         <View
           style={{
             padding: 16,
@@ -138,22 +99,14 @@ const rewards = ()=> {
             justifyContent: "space-between",
           }}
         >
-          <MaterialCommunityIcons
-            name="bell-outline"
-            size={30}
-            style={{ color: colors.white }}
-          />
+          
           <AntDesign name="user" size={30} style={{ color: colors.white }} />
         </View>
       </View>
       <View style={{ padding: 16 }}>
-        <Text style={{ color: "black", fontSize: 30 }}>{"Tasks"}</Text>
-        {/* icone search */}
-        {/* <MaterialCommunityIcons
-            name="magnify"
-            size={30}
-            style={{ color: colors.white }}
-          />  */}
+        <Text style={{ color: "black", fontSize: 30 }}>{"Rewards"}</Text>
+
+    
       </View>
       <View
         style={{
@@ -164,16 +117,33 @@ const rewards = ()=> {
           alignItems: "center",
           borderTopLeftRadius: 20,
         }}
-      ></View>
+      >
+        {/* <TextInput
+          style={{ fontSize: 24 }}
+          onChangeText={(task) => setAddData(task)}
+          placeholder="add new task"
+        /> */}
+        {/* <MaterialCommunityIcons
+          name="plus"
+           onPress={addTodo}
+          size={40}
+          style={{
+            color: colors.themeColor,
+            backgroundColor: colors.white,
+            borderRadius: 20,
+            marginHorizontal: 8,
+          }}
+        /> */}
+      </View>
       <ScrollView
         style={{
           backgroundColor: colors.background,
         }}
       >
-        {todos.map((task) => {
-          {
-            console.log(task);
-          }
+        {rwd.map((rwd) => {
+          // {
+          //   console.log(task);
+          // }
           return (
             <View
               style={{
@@ -186,137 +156,343 @@ const rewards = ()=> {
                 paddingHorizontal: 24,
                 alignItems: "center",
                 justifyContent: "space-between",
-                
               }}
-             
             >
-                
               <View style={{ flexDirection: "row" }}>
+                
                 <View>
-                  <Text style={{ fontSize: 19, textDecorationLine:linefalse(task) }} >{task.task}</Text>
-                  <Text style={{ cololr: colors.greyish }}>
-                    {task.stamp.toString()}
-                  </Text>
+                  <Text style={{ fontSize: 19 }}>{rwd.description}</Text>
+                  
                 </View>
               </View>
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                {/* check icon */}
-                
-                <BouncyCheckbox
-                  size={25}
-                  fillColor="#00BFA6"
-                  unfillColor="rgba(0, 191, 166, 0.15)"
-                  style={{ marginLeft: 30 }}
-                  iconStyle={{ borderColor: "red" }}
-                  innerIconStyle={{ borderWidth: 1 }}
-                  isChecked={task.check}
-                  onPress={()=>updateCheck(task)}
-                  
-                />
-                
-              </View>
+              
             </View>
-           
           );
-          
         })}
-        
       </ScrollView>
-      </View>):
-      
-      (
-        <View style={styles.container}>
-            {/* <Text  >{todos[0]}</Text> */}
-          <RewardsComponent
-            animationType="confetti"
-            state={animationState}
-            onRest={() => setAnimationState('rest')
-        }
-          >
-               <RewardsComponent
-            animationType="confetti"
-            state={animationState}
-            onRest={() => setAnimationState('rest')}
-
-          >
-           
-          </RewardsComponent>
-          {/* button of rewards */}
-            
-            {/* <Text >congratulation Check your rewards </Text> */}
-            
-            <Button
-        title="congratulation Check your rewards"
-        color="green"
-        onPress={() => alert('replace by navigation.navigate')}
-      />
-          </RewardsComponent>
-
-        </View>
-        
-    )} 
-
-      {/* <Text> Lets get started</Text>     */}
+     
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        margin: 10,
-        marginTop: 20,
-      },
-      button: {
-        backgroundColor: CornflowerBlue,
-        height: 60,
-        width: 60,
-        borderRadius: 30,
-        alignItems: 'center',
-        justifyContent: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-      },
-      buttonPunish: {
-        backgroundColor: '#f04',
-        height: 60,
-        width: 60,
-        borderRadius: 30,
-        alignItems: 'center',
-        justifyContent: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        marginTop: 10,
-      },
-      buttonProps: {
-        backgroundColor: 'gray',
-        height: 60,
-        width: 60,
-        alignItems: 'center',
-        justifyContent: 'center',
+// ////5idmet ahlem tasks child illi imiss no9tlouuuuuuuuuuu//////////////
+
+// import * as React from "react";
+// import {  Component, createRef ,useState,useEffect } from "react";
+// import { View, Text, StatusBar, style, Pressable,Platform, StyleSheet, TouchableOpacity,Button } from "react-native";
+// import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
+// import { ScrollView, TextInput } from "react-native-gesture-handler";
+// import { firebase } from "../config/firebase.js";
+// import BouncyCheckbox from "react-native-bouncy-checkbox";
+// import RewardsComponent from 'react-native-rewards';
+
+
+// const CornflowerBlue = '#6495ED';
+
+// const colors = {
+//   themeColor: "#00BFA6",
+//   white: "#fff",
+//   background: "#f4f6fc",
+//   greyish: "#a4a4a4",
+//   tint: "#2b49c3",
+// };
+// // dummy data
+// const Lists = [
+//   {
+//     task: "Do your homework page 90",
+//     icon: "hiking",
+//     theme: "#37003c",
+//     stamp: "Today . 8pm",
+//   },
+//   {
+//     task: "Take care of your sister",
+//     icon: "account-tie",
+//     theme: "#008b8b",
+//     stamp: "Today . 8pm",
+//   },
+//   {
+//     task: "Go to school at 10am",
+//     icon: "cart",
+//     theme: "#fed132",
+//     stamp: "Today . 8pm",
+//   },
+//   {
+//     task: "It's lunch time",
+//     icon: "weight",
+//     theme: "#fed132",
+//     stamp: "Today . 12am",
+//   },
+// ];
+
+// export default function List(props) {
+//   // Declaring const where to save data
+//   //Delete from firestore
+//   //fetch data
+//   const [todos, setTodos] = useState([]);
+
+//   // collect collection tasks from firebase
+//   const todoRef = firebase.firestore().collection("tasks");
+
+//   const [addData, setAddData] = useState("");
+//   const [animationState,setAnimationState] =useState('rest')
+
+//   //Get data from firestore
+//   useEffect(() => {
+//       setAnimationState("reward")
+
+//     todoRef.orderBy("stamp", "desc").onSnapshot((QuerySnapshot) => {
+//       const todos = [];
+//       QuerySnapshot.forEach((doc) => {
+//         //  console.log(doc)
+//         const { task, icon, theme, stamp,check,idChild,idParent} = doc.data();
+//         // console.log(doc.data())
+//         // if (idChild==="222"){
+//         todos.push({
+//           id: doc.id,
+//           task,
+//           icon,
+//           theme,
+//           stamp,
+//           check,
+//           idChild,
+//           idParent,
+//         });
+//     // }
+//       });
+//       setTodos(todos);
+//     });
+   
+//   }, []);
+  
+//   // function to check true or false
+//   const updateCheck = (task) => {
+//     todoRef.doc(task.id).update({
+//       check: !task.check,
+//     }).then(()=>{
+//     console.log(task)
+//    })
+  
+//     .catch(error=>alert('ee',error))
+//   };
+
+//   // function linefalse
+//   const linefalse=(task)=>{
+//     if (task.check===true){
+//         return "line-through"
+//     }
+//     return "none"
+//   }
+  
+
+
+// //function rewards
+// const rewards = ()=> {
+//     var res=true
+//     for (var i =0 ;i<todos.length;i++){
+//          if (todos[i].check===false){
+//        res= false
+//        }
+//      }
+//    console.log('all todos are done')
+//      return  res   }
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         background: colors.themeColor,
+//       }}
+//     >
+//       {/* display data from firebase */}
+//       <StatusBar barStyle="light-content" backgroundColor={colors.themeColor} />
+//      {rewards() ===false ?(
+//      <View>
+//      <View style={{ backgroundColor: colors.themeColor }}>
+//         <View
+//           style={{
+//             padding: 16,
+//             flexDirection: "row",
+//             justifyContent: "space-between",
+//           }}
+//         >
+//           <MaterialCommunityIcons
+//             name="bell-outline"
+//             size={30}
+//             style={{ color: colors.white }}
+//           />
+//           <AntDesign name="user" size={30} style={{ color: colors.white }} />
+//         </View>
+//       </View>
+//       <View style={{ padding: 16 }}>
+//         <Text style={{ color: "black", fontSize: 30 }}>{"Tasks"}</Text>
+//         {/* icone search */}
+//         {/* <MaterialCommunityIcons
+//             name="magnify"
+//             size={30}
+//             style={{ color: colors.white }}
+//           />  */}
+//       </View>
+//       <View
+//         style={{
+//           padding: 20,
+//           flexDirection: "row",
+//           backgroundColor: colors.background,
+//           justifyContent: "space-between",
+//           alignItems: "center",
+//           borderTopLeftRadius: 20,
+//         }}
+//       ></View>
+//       <ScrollView
+//         style={{
+//           backgroundColor: colors.background,
+//         }}
+//       >
+//         {todos.map((task) => {
+//           {
+//             console.log(task);
+//           }
+//           return (
+//             <View
+//               style={{
+//                 backgroundColor: colors.white,
+//                 flexDirection: "row",
+//                 marginHorizontal: 16,
+//                 marginVertical: 4,
+//                 borderRadius: 20,
+//                 paddingVertical: 20,
+//                 paddingHorizontal: 24,
+//                 alignItems: "center",
+//                 justifyContent: "space-between",
+                
+//               }}
+             
+//             >
+                
+//               <View style={{ flexDirection: "row" }}>
+//                 <View>
+//                   <Text style={{ fontSize: 19, textDecorationLine:linefalse(task) }} >{task.task}</Text>
+//                   <Text style={{ cololr: colors.greyish }}>
+//                     {task.stamp.toString()}
+//                   </Text>
+//                 </View>
+//               </View>
+//               <View style={{ flexDirection: "row", alignItems: "center" }}>
+//                 {/* check icon */}
+                
+//                 <BouncyCheckbox
+//                   size={25}
+//                   fillColor="#00BFA6"
+//                   unfillColor="rgba(0, 191, 166, 0.15)"
+//                   style={{ marginLeft: 30 }}
+//                   iconStyle={{ borderColor: "red" }}
+//                   innerIconStyle={{ borderWidth: 1 }}
+//                   isChecked={task.check}
+//                   onPress={()=>updateCheck(task)}
+                  
+//                 />
+                
+//               </View>
+//             </View>
+           
+//           );
+          
+//         })}
+        
+//       </ScrollView>
+//       </View>):
+      
+//       (
+//         <View style={styles.container}>
+//             {/* <Text  >{todos[0]}</Text> */}
+//           <RewardsComponent
+//             animationType="confetti"
+//             state={animationState}
+//             onRest={() => setAnimationState('rest')
+//         }
+//           >
+//                <RewardsComponent
+//             animationType="confetti"
+//             state={animationState}
+//             onRest={() => setAnimationState('rest')}
+
+//           >
+           
+//           </RewardsComponent>
+//           {/* button of rewards */}
+            
+//             {/* <Text >congratulation Check your rewards </Text> */}
+            
+//             <Button
+//         title="congratulation Check your rewards"
+//         color="green"
+//         onPress={() => alert('replace by navigation.navigate')}
+//       />
+//           </RewardsComponent>
+
+//         </View>
+        
+//     )} 
+
+//       {/* <Text> Lets get started</Text>     */}
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//       container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: '#fff',
+//         margin: 10,
+//         marginTop: 20,
+//       },
+//       button: {
+//         backgroundColor: CornflowerBlue,
+//         height: 60,
+//         width: 60,
+//         borderRadius: 30,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         shadowColor: '#000',
+//         shadowOffset: {
+//           width: 0,
+//           height: 2,
+//         },
+//         shadowOpacity: 0.25,
+//         shadowRadius: 3.84,
+//         elevation: 5,
+//       },
+//       buttonPunish: {
+//         backgroundColor: '#f04',
+//         height: 60,
+//         width: 60,
+//         borderRadius: 30,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         shadowColor: '#000',
+//         shadowOffset: {
+//           width: 0,
+//           height: 2,
+//         },
+//         shadowOpacity: 0.25,
+//         shadowRadius: 3.84,
+//         elevation: 5,
+//         marginTop: 10,
+//       },
+//       buttonProps: {
+//         backgroundColor: 'gray',
+//         height: 60,
+//         width: 60,
+//         alignItems: 'center',
+//         justifyContent: 'center',
        
-        marginTop: 10,
-      },
-      buttonText: {
-        color: 'white',
-        fontSize: 24,
-      },
-    });
+//         marginTop: 10,
+//       },
+//       buttonText: {
+//         color: 'white',
+//         fontSize: 24,
+//       },
+//     });
 
 
 
